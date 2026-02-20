2e6d312ab29e38cdd53711a3cf42afddb804c341
drzaki94 <dr.muhammadzaki@moh.gov.my>
2026-02-20 11:51:55 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,49 +1,21 @@
-const CACHE_NAME = 'gcfm-mastery-v2';
-
-// USE RELATIVE PATHS FOR GITHUB PAGES (./ instead of /)
-const ASSETS_TO_CACHE = [
-  './',
+const CACHE_NAME = 'gcfm-v1';
+const ASSETS = [
   './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
   './gcfm_part1.html',
   './gcfm_part2.html',
   './gcfm_part3.html',
   './gcfm_part4.html',
   './gcfm_part5.html',
-  './gcfm_part6.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  './gcfm_part6.html'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('[Service Worker] Caching all assets');
-        return cache.addAll(ASSETS_TO_CACHE);
-      })
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          console.log('[Service Worker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-  return self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
-      })
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
