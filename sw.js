3d5a777acdde24001774b3791c84eec86eefa7a8
drzaki94 <dr.muhammadzaki@moh.gov.my>
2026-02-20 11:35:44 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,20 @@
-// Name of the cache
-const CACHE_NAME = 'gcfm-mastery-v1';
+const CACHE_NAME = 'gcfm-mastery-v2';
 
-// List of files to cache for offline use
+// USE RELATIVE PATHS FOR GITHUB PAGES (./ instead of /)
 const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/gcfm_part1.html',
-  '/gcfm_part2.html',
-  '/gcfm_part3.html',
-  '/gcfm_part4.html',
-  '/gcfm_part5.html',
-  '/gcfm_part6.html',
-  '/manifest.json',
-  '/icon.png'
+  './',
+  './index.html',
+  './gcfm_part1.html',
+  './gcfm_part2.html',
+  './gcfm_part3.html',
+  './gcfm_part4.html',
+  './gcfm_part5.html',
+  './gcfm_part6.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-// Install Event: Cache all the files
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -26,7 +25,6 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// Activate Event: Clean up old caches
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keyList) => {
@@ -38,16 +36,13 @@ self.addEventListener('activate', (event) => {
       }));
     })
   );
-  // Force the service worker to take control immediately
   return self.clients.claim();
 });
 
-// Fetch Event: Serve cached files when offline
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
-        // If the file is in cache, return it. Otherwise, fetch it from the network.
         return response || fetch(event.request);
       })
   );
